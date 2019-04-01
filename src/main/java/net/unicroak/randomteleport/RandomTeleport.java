fa03745a861c184271fd5b0fe9f02cd627c4f44b
unicroak <unicroak@gmail.com>
2019-04-01 22:36:37 +0900

    initialize project

---
+++ src/main/java/net/unicroak/randomteleport/RandomTeleport.java
@@ -0,0 +1,20 @@
+package net.unicroak.randomteleport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author unicroak
+ */
+public final class RandomTeleport extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
